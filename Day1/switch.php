dc086ab711c9fb2e7288fed3b9c716c21a9ba654
NguyenQuangTuan02 <129353107+283pan@users.noreply.github.com>
2024-04-16 08:22:29 +0700

    Update switch.php

--- Day1/switch.php
+++ Day1/switch.php
@@ -3,7 +3,29 @@
 
     <body>
     <?php
-    
+    $nhap = "t2";
+
+        switch($nhap){
+                case "t1":
+                case "t3":
+                case "t5":
+                case "t7":
+                case "t8":
+                case "t10":
+                case "t12":
+                    echo "Tháng có 31 ngày";
+                    break;
+                
+                case "t4":
+                case "t6":
+                case "t9":
+                case "t11":
+                    echo "thang co 30 ngay";
+                break;
+            case("t2"):
+                echo "thang co 28 ngay";
+                break;
+        }
     ?>
     </body>
 </html>
